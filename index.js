b93893572823ec0900b8bea2152d994b703d3b1f
Mister-Kay <28190493+Mister-Kay@users.noreply.github.com>
2017-11-19 09:23:13 -0600

    Add UI sound on reset

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const SHOW_SYSTEM_RESET_MESSAGE = false;
 const RESET_FONT_COLOR = '#FF4500'; // https://www.google.com/search?q=colour+picker
 const FLASHING_NOTIFICATION = false;
+const SOUND_ID = 3028;
+const PLAY_SOUND = true;
 
 module.exports = function SkillResets(dispatch) {
 	let model;
@@ -15,10 +17,17 @@ module.exports = function SkillResets(dispatch) {
 			unk3: 0
 		});
 	};
+	
+	function playSound(id) {
+		dispatch.toClient('S_PLAY_SOUND', 1, {
+			SoundID: id
+		})
+	}
 
 	dispatch.hook('S_CREST_MESSAGE', 1, event => {
 		if (event.type === 6) {
 			showMessage(`<img src="img://skill__0__${model}__${event.skillID}" width="48" height="48" vspace="-20"/><font size="24" color="${RESET_FONT_COLOR}">&nbsp;Reset</font>`);
+			if (PLAY_SOUND) playSound(SOUND_ID);
 			if (!SHOW_SYSTEM_RESET_MESSAGE) return false;
 		}
 	});
